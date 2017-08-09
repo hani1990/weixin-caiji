2684a5f1feeb455049b761cb4eb7a67d0906a818
hani1990 <1121797386@qq.com>
2017-08-09 10:09:01 +0800

    Update getMsgJson.php

--- getMsgJson.php
+++ getMsgJson.php
@@ -22,50 +22,63 @@ if(!$json) {
 
     $json = json_decode(htmlspecialchars_decode($str), true);//如果不成功，就增加一步htmlspecialchars_decode
 }
+
     //$json 不为空才执行, 注入操作
     foreach ($json['list'] as $k => $v) {
         $type = $v['comm_msg_info']['type'];
         if ($type == 49) {//type=49代表是图文消息
             $weixin->log(49);
-            $content_url = str_replace("\\", "", htmlspecialchars_decode($v['app_msg_ext_info']['content_url']));//获得图文消息的链接地址
+           
             $is_multi = $v['app_msg_ext_info']['is_multi'];//是否是多图文消息
             $datetime = $v['comm_msg_info']['datetime'];//图文消息发送时间
-            //在这里将图文消息链接地址插入到采集队列库中（队列库将在后文介绍，主要目的是建立一个批量采集队列，另一个程序将根据队列安排下一个采集的公众号或者文章内容）
-            $weixin->addQueue($content_url);
+
+                //在这里将图文消息链接地址插入到采集队列库中（队列库将在后文介绍，主要目的是建立一个批量采集队列，另一个程序将根据队列安排下一个采集的公众号或者文章内容）
+            
             $id = $v['comm_msg_info']['id'];
             //todo 遍历 id-10 插入队列
+            if($is_multi == 0){
+                unset($content_url);
+                $content_url = str_replace("\\", "", htmlspecialchars_decode($v['app_msg_ext_info']['content_url']));//获得图文消息的链接地址
+                $weixin->addQueue($content_url);
 
-            //在这里根据$content_url从数据库中判断一下是否重复
-            if (!$weixin->exitContentUrl($content_url)) {
-                $fileid = $v['app_msg_ext_info']['fileid'];//一个微信给的id
-                $title = $v['app_msg_ext_info']['title'];//文章标题
-                $title_encode = urlencode(str_replace("&nbsp;", "", $title));//建议将标题进行编码，这样就可以存储emoji特殊符号了
-                $digest = $v['app_msg_ext_info']['digest'];//文章摘要
-                $source_url = str_replace("\\", "", htmlspecialchars_decode($v['app_msg_ext_info']['source_url']));//阅读原文的链接
-                $cover = str_replace("\\", "", htmlspecialchars_decode($v['app_msg_ext_info']['cover']));//封面图片
+                //在这里根据$content_url从数据库中判断一下是否重复
+                if (!$weixin->exitContentUrl($content_url)) {
 
-                $is_top = 1;//标记一下是头条内容
-                //现在存入数据库
-                $data['biz'] = $biz;
-                $data['field_id'] = $fileid;
-                $data['title'] = $title;
-                $data['title_encode'] = $title_encode;
-                $data['digest'] = $digest;
-                $data['source_url'] = $source_url;
-                $data['cover'] = $cover;
-                $data['content_url'] = $content_url;
-                //$data['is_multi'] = $is_multi;
-                $data['datetime'] = intval($datetime);
-                $lastId = $weixin->addPost($data);
+                    $fileid = $v['app_msg_ext_info']['fileid'];//一个微信给的id
+                    $title = $v['app_msg_ext_info']['title'];//文章标题
+                    $title_encode = urlencode(str_replace("&nbsp;", "", $title));//建议将标题进行编码，这样就可以存储emoji特殊符号了
+                    $digest = $v['app_msg_ext_info']['digest'];//文章摘要
+                    $source_url = str_replace("\\", "", htmlspecialchars_decode($v['app_msg_ext_info']['source_url']));//阅读原文的链接
+                    $cover = str_replace("\\", "", htmlspecialchars_decode($v['app_msg_ext_info']['cover']));//封面图片
 
-                echo "头条标题：" . $title . $lastId . "\n";//这个echo可以显示在anyproxy的终端里
-            }
-            if ($is_multi == 1) {//如果是多图文消息
+                    $is_top = 1;//标记一下是头条内容
+                    //现在存入数据库
+                    unset($data);
+                    $data['biz'] = $biz;
+                    $data['field_id'] = $fileid;
+                    $data['title'] = $title;
+                    $data['title_encode'] = $title_encode;
+                    $data['digest'] = $digest;
+                    $data['source_url'] = $source_url;
+                    $data['cover'] = $cover;
+                    $data['content_url'] = $content_url;
+                    //$data['is_multi'] = $is_multi;
+                    $data['datetime'] = intval($datetime);
+                    $data['is_multi'] = $is_multi;
+                    $lastId = $weixin->addPost($data);
+
+
+                    echo "头条标题：" . $title . $lastId . "\n";//这个echo可以显示在anyproxy的终端里
+                }
+            } else {//如果是多图文消息
                 $weixin->log(1);
+                unset($data);
                 foreach ($v['app_msg_ext_info']['multi_app_msg_item_list'] as $kk => $vv) {//循环后面的图文消息
+                    unset($content_url);
                     $content_url = str_replace("\\", "", htmlspecialchars_decode($vv['content_url']));//图文消息链接地址
+                     $weixin->addQueue($content_url);
                     //这里再次根据$content_url判断一下数据库中是否重复以免出错
-                    if ('数据库中不存在相同的$content_url') {
+                    if (!$weixin->exitContentUrl($content_url)) {
                         //在这里将图文消息链接地址插入到采集队列库中（队列库将在后文介绍，主要目的是建立一个批量采集队列，另一个程序将根据队列安排下一个采集的公众号或者文章内容）
                         $title = $vv['title'];//文章标题
                         $fileid = $vv['fileid'];//一个微信给的id
@@ -75,12 +88,15 @@ if(!$json) {
                         //$cover = getCover(str_replace("\\","",htmlspecialchars_decode($vv['cover'])));
                         $cover = str_replace("\\", "", htmlspecialchars_decode($vv['cover']));//封面图片
                         //现在存入数据库
+                        $data['biz'] = $biz;
                         $data['title'] = $title;
                         $data['field_id'] = $fileid;
                         $data['title_encode'] = $title_encode;
                         $data['digest'] = $digest;
                         $data['source_url'] = $source_url;
                         $data['cover'] = $cover;
+                        $data['content_url'] = $content_url;
+                        $data['is_multi'] = $is_multi;
                         $lastId = $weixin->addPost($data);
                         echo "标题：" . $title . $lastId . "\n";
                     }
@@ -89,4 +105,13 @@ if(!$json) {
             }
         }
     }
+
+
+            // $sql = "select * from weixin order by collect asc";
+            // $weixin = $db->get_one($sql);
+            // //update 
+            // $db->update("weixin", ['collect' => time()], "`id` = ". $weixin['id']);
+            // $biz = $weixin['biz'];
+            // $url = "http://mp.weixin.qq.com/mp/getmasssendmsg?__biz=".$biz."#wechat_webview_type=1&wechat_redirect";
+            // echo '<meta http-equiv=refresh content="3;url='.$url.'">';
 ?>
